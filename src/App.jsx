53b6b357c76162a5bde548113a655e80a8b77e54
faazdy <leorengifor@gmail.com>
2025-03-26 00:52:26 -0500

    not found page

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,22 @@ import Navbar from "./components/Navbar";
 import Shadows from "./pages/shadows/ShadowsList";
 import ButtonList from "./pages/buttons/ButtonList";
 import CardList from "./pages/cards/CardList";
+import Notfound from "./pages/NotFound/Notfound";
 import Footer from "./components/Footer";
-
+import ScrollToTop from "./components/ScrollTop";
 
 function App() {
   return (
     <>
       <Router >
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shadows" element={<Shadows />} />
           <Route path="/buttons" element={<ButtonList />} />
           <Route path="/cards" element={<CardList />} />
+          <Route path="/*" element={<Notfound />} />
         </Routes>
         <Footer/>
       </Router>
