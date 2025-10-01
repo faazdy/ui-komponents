0eaff6fd72024a9d029765e19992473986ba36b2
Leonardo <158385461+faazdy@users.noreply.github.com>
2025-10-01 18:52:14 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,7 +10,7 @@ function Footer() {
         <a href="https://github.com/faazdy">
           <img src="assets/icons/github.png" alt="git" />
         </a>
-        <h3>Project by: <a href="https://fazdev-front.netlify.app/">Leonardo</a></h3>
+        <h3>Project by: <a href="https://leorengifodev.vercel.app/">Leonardo</a></h3>
         <p>Front-end Dev</p>
         <span>2025</span>
       </div>
